4e04e86f5713044c2f51af33af7f709d0717f632
LuybaKo <Born_Luyba@mail.ru>
2019-06-02 11:45:46 +0300

    Change navigation in index and common.js

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,22 @@ function myFunction(x) {
 	x.classList.toggle("change");
 }
 
+var burger = document.getElementById("burger");
+burger.onclick = function () {
+	var hiddenLinks = document.querySelectorAll(".hide");
+	for (var i = 0; i < hiddenLinks.length; i++) {
+		var hiddenLink = hiddenLinks[i];
+		if (hiddenLink.style.visibility === 'hidden') {
+			hiddenLink.style.visibility = 'visible';
+		} else {
+			hiddenLink.style.visibility = 'hidden';
+		}
+	}
+}
+
+
+
+
 // $(function() {      
 // 	//Enable swiping...
 // 	$("#test").swipe( {
